0bf81b4688c418f97c9be6e3f7984df172d96d48
Nidhin K R <www.nidhinqzee7@gmail.com>
2025-07-06 11:21:04 +0530

    state

---
+++ lib/viewmodel/recipe_viewmodel.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/models/recipe_model.dart';
+import 'package:my_project/repository/recipe_repository.dart';
+
+class RecipeViewModel extends ChangeNotifier {
+  final RecipeRepository repository;
+
+  RecipeViewModel({required this.repository});
+
+  List<RecipeModel> _recipes = [];
+  bool _isLoading = false;
+  String? _error;
+
+  List<RecipeModel> get recipes => _recipes;
+  bool get isLoading => _isLoading;
+  String? get error => _error;
+
+  Future<void> fetchRecipes() async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      _recipes = await repository.getAllRecipes();
+      notifyListeners();
+    } catch (e) {
+      _error = e.toString();
+    }
+
+    _isLoading = false;
+    notifyListeners();
+  }
+}
